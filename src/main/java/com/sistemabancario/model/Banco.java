c8195d81b58d96daa35e2866cc8bee7e006657a3
Manoel Campos <manoelcampos@gmail.com>
2021-02-10 14:28:23 -0300

    Atualiza docs do sistema bancário.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/sistemabancario/model/Banco.java
+++ src/main/java/com/sistemabancario/model/Banco.java
@@ -23,12 +23,11 @@ public class Banco implements Cadastro {
      *      O tipo é String para permitir restringir o total de caracteres, mas deve
      *      ser informado um número válido (somente dígitos de 0 a 9). Senão for um
      *      número válido, uma exceção deve ser lançada informando o problema. 
-     *  
-     *      (R02)
      *  </li>
      *  <li>
      *      É um atributo obrigatório, não podendo ser nulo nem vazio. 
      *      Se não for preenchido, uma exceção deve ser lançada informando o problema. 
+     *      (R02)
      *  </li>
      * </ul>
      */
